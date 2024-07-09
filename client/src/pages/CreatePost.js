733ab8fea23e3436affc12186c84bf9387a29d3b
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-09 13:03:47 +0100

    commit

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import {Formik, Form, Field, ErrorMessage } from 'formik';
 
 function CreatePost() {
   return (
-    <div>CreatePost</div>
+    <div className='createPostPage'>
+        <Formik>
+            <Form className='formContainer' >
+                <label>Title: </label>
+                <Field 
+                autocomplete="off"
+                id="inputCreatePost" 
+                name="title" 
+                placeholder="(Ex. khouloud....)"/>
+
+                <label>Post: </label>
+                <Field 
+                autocomplete="off"
+                id="inputCreatePost" 
+                name="postText" 
+                placeholder="(Ex. post....)"/>
+
+                
+                <label>UserName: </label>
+                <Field 
+                autocomplete="off"
+                id="inputCreatePost" 
+                name="userName" 
+                placeholder="(Ex. name....)"/>
+
+                <button type='submit'>Create Post</button>
+            </Form>
+        </Formik>
+        </div>
   )
 }
 
