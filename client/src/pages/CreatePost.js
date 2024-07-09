4df80e5dca84153b2984a8a3f70e25d73a629128
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-09 13:44:53 +0100

    commit

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,62 +1,64 @@
-import React from 'react';
-import {Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
 function CreatePost() {
-           const initialValues = {
-            title:"",
-            PostText:"",
-            userName:"",
-           }
+  const initialValues = {
+    title: "",
+    postText: "",
+    userName: "",
+  };
 
-           const onSubmit = (data) =>{
-            axios.post('http://localhost:3001/Posts',data).then((response) => {
-               console.log("TI Worked");
-           });
-        };
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("You must input a Title!"),
+    postText: Yup.string().required(),
+    userName: Yup.string().min(3).max(15).required(),
+  });
 
-           const validationSchema = Yup.object().shape({
-            title: Yup.string().required(),
-            PostText:Yup.string().required(),
-            userName:Yup.string().min(3).max(15).required(),
-           });
-
-
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/Posts", data).then((response) => {
+      console.log("IT WORKED");
+    });
+  };
   return (
-    <div className='createPostPage'>
-        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-            <Form className='formContainer' >
-                <label>Title: </label>
-                <ErrorMessage name="title" component="span" />
-                <Field 
-                autocomplete="off"
-                id="inputCreatePost" 
-                name="title" 
-                placeholder="(Ex. title....)"/>
+    <div className="createPostPage">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Title: </label>
+          <ErrorMessage name="title" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="title"
+            placeholder="(Ex. Title...)"
+          />
+          <label>Post: </label>
+          <ErrorMessage name="postText" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="postText"
+            placeholder="(Ex. Post...)"
+          />
+          <label>Username: </label>
+          <ErrorMessage name="userName" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="userName"
+            placeholder="(Ex. khouloud...)"
+          />
 
-                <label>Post: </label>
-                <ErrorMessage name="postText" component="span" />
-                <Field 
-                autocomplete="off"
-                id="inputCreatePost" 
-                name="postText" 
-                placeholder="(Ex. post....)"/>
-
-                
-                <label>UserName: </label>
-                <ErrorMessage name="userName" component="span" />
-                <Field 
-                autocomplete="off"
-                id="inputCreatePost" 
-                name="userName" 
-                placeholder="(Ex. name....)"/>
-
-                <button type='submit'>Create Post</button>
-            </Form>
-        </Formik>
-        </div>
-  )
+          <button type="submit"> Create Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
