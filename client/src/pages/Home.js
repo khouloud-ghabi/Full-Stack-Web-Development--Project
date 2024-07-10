a36b90d81c501c23d413e74d0521b6f17eb93c60
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-10 14:39:21 +0100

    commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
   const [listOfPosts, setListOfPosts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get('http://localhost:3001/Posts').then((response) => {
@@ -14,7 +16,11 @@ function Home() {
     <div>
       {listOfPosts.map((value, key) => {
         return (
-          <div className='post' key={key}>
+          <div 
+            key={key} 
+            className='post' 
+            onClick={() => navigate(`/post/${value.id}`)}
+          >
             <div className='title'>{value.title}</div>
             <div className='body'>{value.postText}</div>
             <div className='footer'>{value.userName}</div>
