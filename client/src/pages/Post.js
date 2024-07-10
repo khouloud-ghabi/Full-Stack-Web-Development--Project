6b533be35c146132eb5944d4b3476d1a60d7434f
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-10 15:17:44 +0100

    commit

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,22 +1,27 @@
-import React ,{useEffect, useState} from 'react';
-import {useParams} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
-function Post() {
-    let {id} = useParams();
-    const [postObject,setPostObject] = useState({});
-    useEffect(() => {
-        axios.get('http://localhost:3001/Posts/byId/${id}').then((response) => {
-        setPostObject(response.data);
-        });
-      });
 
+function Post() {
+  let { id } = useParams();
+  const [postObject, setPostObject] = useState({});
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/Posts/byId/${id}`).then((response) => {
+      setPostObject(response.data);
+    });
+  }, [id]);
 
   return (
     <div className='postPage'>
-      <div className='leftSide'></div>
-      <div className='rightSide'></div>
+      <div className='leftSide'>
+        <div className='title'>{postObject.title}</div>
+        <div className='body'>{postObject.postText}</div>
+        <div className='footer'>{postObject.userName}</div>
       </div>
-  )
+      <div className='rightSide'>Comment Section</div>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
