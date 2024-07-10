64a1120d22f970f593515be01e0247afa5eb2664
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-10 14:24:21 +0100

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes,Link } from 'react-router-dom';
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
+import Post from "./pages/Post";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <div className='navbar'>
         <Link to ="createpost">Create A Post</Link>
         <Link to ="/">Home Page</Link>
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/createpost" element={<CreatePost />} />
+          <Route path="/post/:id" element={<Post />} />
         </Routes>
       </Router>
     </div>
