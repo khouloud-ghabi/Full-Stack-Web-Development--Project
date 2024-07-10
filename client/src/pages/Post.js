d12f71ceffc09d5203ec76a9b06dd669bd3b9b4d
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-10 15:10:20 +0100

    commit

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React ,{useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-
+import axios from 'axios';
 function Post() {
     let {id} = useParams();
+    const [postObject,setPostObject] = useState({});
+    useEffect(() => {
+        axios.get('http://localhost:3001/Posts/byId/${id}').then((response) => {
+        setPostObject(response.data);
+        });
+      });
+
+
   return (
-    <div>{id}</div>
+    <div className='postPage'>
+      <div className='leftSide'></div>
+      <div className='rightSide'></div>
+      </div>
   )
 }
 
