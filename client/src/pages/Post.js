6de29e035ab58e7b7a1a783c91972acab36d1fd0
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-10 14:26:46 +0100

    commit

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import {useParams} from "react-router-dom";
 
 function Post() {
+    let {id} = useParams();
   return (
-    <div>Post</div>
+    <div>{id}</div>
   )
 }
 
