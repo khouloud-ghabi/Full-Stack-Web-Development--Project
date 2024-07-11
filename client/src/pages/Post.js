3a10247868b82461df02521eeeb4317494457405
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-11 16:42:55 +0100

    commit

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -5,12 +5,38 @@ import axios from 'axios';
 function Post() {
   let { id } = useParams();
   const [postObject, setPostObject] = useState({});
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
 
   useEffect(() => {
     axios.get(`http://localhost:3001/Posts/byId/${id}`).then((response) => {
       setPostObject(response.data);
     });
+
+    axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+      if (Array.isArray(response.data)) {
+        setComments(response.data);
+      } else {
+        console.error("Fetched comments are not an array:", response.data);
+        setComments([]);
+      }
+    }).catch(error => {
+      console.error("Error fetching comments:", error);
+      setComments([]);
+    });
   }, [id]);
+
+  const addComment = () => {
+    axios.post(`http://localhost:3001/comments`, {
+      PostId: id,
+      commentBody: newComment
+    }).then((response) => {
+      setComments([...comments, response.data]);
+      setNewComment("");
+    }).catch(error => {
+      console.error("Error adding comment:", error);
+    });
+  };
 
   return (
     <div className='postPage'>
@@ -21,14 +47,22 @@ function Post() {
       </div>
       <div className='rightSide'>
         <div className='addCommentContainer'>
-        <input
+          <input
             type="text"
             placeholder="Comment..."
             autoComplete="off"
-            />
-          <button > Add Comment</button>
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+          />
+          <button onClick={addComment}>Add Comment</button>
         </div>
-        <div className='listOfComments'></div>
+        <div className="listOfComments">
+          {comments.map((comment, key) => (
+            <div key={key} className="comment">
+              {comment.commentBody}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
