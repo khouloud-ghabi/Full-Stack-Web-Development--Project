30ae3cb8fd230853fac3ceba2e57c9688cffa03b
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-11 17:32:17 +0100

    commit

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,24 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {Users} = require("../models"); 
-const bcrypt = require ("bcrypt");
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
 
-
-router.post('/',async (req,res)=>{
-    const {userName, password} = req.body;
-    bcrypt.hash(password,10).then((hash)=>{
-       Users.create({
-        userName : userName,
-        password: hash,
-       });
-       res.json("Success")
-    })
+router.post("/", async (req, res) => {
+  const { userName, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      userName: userName,
+      password: hash,
+    });
+    res.json("SUCCESS");
+  });
 });
 
-router.post('/login',async(req,res)=>{
-    const {userName, password} = req.body;
-    const user = await Users.findOne({where: { userName:userName } });
-    if (!user) res.json({ error: "User Doesn't Exist" });
+router.post("/login", async (req, res) => {
+  const { userName, password } = req.body;
+
+  const user = await Users.findOne({ where: { userName: userName } });
+
+  if (!user) res.json({ error: "User Doesn't Exist" });
+
+  bcrypt.compare(password, user.password).then((match) => {
+    if (!match) res.json({ error: "Wrong Username And Password Combination" });
+
+    res.json("YOU LOGGED IN!!!");
+  });
 });
 
 module.exports = router;
