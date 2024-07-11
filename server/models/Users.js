507d7bae862acc98cb8a0be652efc7d39188c508
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-11 16:53:50 +0100

    commit

---
+++ server/models/Users.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize,DataType )=>{
+    //creat a posts table
+    const Users = sequelize.define("Users",{
+   
+    userName:{
+        type:DataType.STRING,
+        allowNull: false,
+    },
+    password:{
+        type:DataType.STRING,
+        allowNull: false,
+    },
+    });
+    
+    Users.associate = (models) =>{
+        Users.hasMany(models.Posts,{
+            onDelete: "cascade",
+    
+        });
+    
+    };
+
+    
+    return Users;
+    };
