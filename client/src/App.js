9d600622dde06faab7e0e21040bea3fc39be1597
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-12 14:19:43 +0100

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes,Link } from 'react-router-dom';
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/createpost" element={<CreatePost />} />
           <Route path="/post/:id" element={<Post />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration/>} />
         </Routes>
       </Router>
     </div>
