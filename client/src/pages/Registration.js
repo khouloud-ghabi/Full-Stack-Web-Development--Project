f5904c04d86172e6e8f70287b2d83fe384e70943
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-12 14:29:34 +0100

    commit

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function Registration() {
+    
+    const initialValues = {
+            userName: "",
+            password:"",
+      };  
+      const validationSchema = Yup.object().shape({
+        userName: Yup.string().min(3).max(15).required(),
+        passsword: Yup.string().min(4).max(20).required(),
+      });
+    
+    
+      const onSubmit=()=>{};
+
   return (
-    <div>Registration</div>
+    <div> <Formik
+    initialValues={initialValues}
+    onSubmit={onSubmit}
+    validationSchema={validationSchema}
+  >
+    <Form className="formContainer">
+      <label>Username: </label>
+      <ErrorMessage name="userName" component="span" />
+      <Field
+        autocomplete="off"
+        id="inputCreatePost"
+        name="userName"
+        placeholder="(your name.....)"
+      />
+      <label>Passsword: </label>
+      <ErrorMessage name="password" component="span" />
+      <Field
+        autocomplete="off"
+        id="inputCreatePost"
+        name="password"
+        placeholder="(your password.....)"
+      />
+
+      <button type="submit"> Register</button>
+    </Form>
+  </Formik></div>
   )
 }
 
